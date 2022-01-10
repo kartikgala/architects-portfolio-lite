93ff38c7d4ac326e9e4bc01d9259ebd3dc86de96
aartsyfaartsy <kartikgala@users.noreply.github.com>
2022-01-10 06:38:09 +0530

    commit

--- index.php
+++ index.php
@@ -14,17 +14,17 @@
 
 get_header(); ?>
 
-		<div id="primary" class="site-content">
-			<div id="content" role="main">
+<div id="primary" class="site-content">
+  <div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
+    <?php if ( have_posts() ) : ?>
 
-				<?php underscoresme_content_nav( 'nav-above' ); ?>
+    <?php underscoresme_content_nav( 'nav-above' ); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+    <?php /* Start the Loop */ ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
+    <?php
 						/* Include the Post-Format-specific template for the content.
 						 * If you want to overload this in a child theme then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
@@ -32,18 +32,18 @@ get_header(); ?>
 						get_template_part( 'content', get_post_format() );
 					?>
 
-				<?php endwhile; ?>
+    <?php endwhile; ?>
 
-				<?php underscoresme_content_nav( 'nav-below' ); ?>
+    <?php underscoresme_content_nav( 'nav-below' ); ?>
 
-			<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
+    <?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
 
-				<?php get_template_part( 'no-results', 'index' ); ?>
+    <?php get_template_part( 'no-results', 'index' ); ?>
 
-			<?php endif; ?>
+    <?php endif; ?>
 
-			</div><!-- #content -->
-		</div><!-- #primary .site-content -->
+  </div><!-- #content -->
+</div><!-- #primary .site-content -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
